51b92e51cd20f2ea7a1e5b04a67609be0bd9cc1a
Mounir Afifi <antontheog@gmail.com>
2020-05-27 21:09:58 +0200

    adding C#

---
+++ hello.cs
@@ -0,0 +1,25 @@
+
+using System;
+namespace test 
+{
+    
+    public class Hello 
+    {
+            public static int Main() 
+            {
+
+                    System.Console.WriteLine("Hello World!");
+                    
+                    return 0;
+            }
+
+
+    }
+
+    
+
+   
+   
+
+}
+
